afa5d0f1093ba3e23011371419eec705027d5351
Adrian Somesan <adrian.somesan@brivo.com>
2025-07-09 17:10:37 +0300

    add all secrets

--- GithubSecurity/GithubSecurity/ContentView.swift
+++ GithubSecurity/GithubSecurity/ContentView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct ContentView: View {
 
     let stripeSecret = "a111aa11-a111-11a1-aaaa-aa1111a11a11"
+    let githubToken = "ghp_1a2b3c4d5e6f7g8h9i0jklmnopqrstuvwx"
+    let awsAccessKey = "AKIAIOSFODNN7EXAMPLE"
+    let awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    let gcpApiKey = "AIzaSyD1x2a3B4C5d6E7F8G9hI0JKlMNOPQRstu"
+    let twilioSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    let twilioToken = "your_auth_token_here_1234567890"
 
     var body: some View {
         VStack {
