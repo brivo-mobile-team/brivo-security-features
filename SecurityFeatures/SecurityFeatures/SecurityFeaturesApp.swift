0662a937dc4fc8812f851516abe8ece1efcce764
Adrian Somesan <adrian.somesan@brivo.com>
2025-07-09 11:51:21 +0300

    added project

---
+++ SecurityFeatures/SecurityFeatures/SecurityFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityFeaturesApp.swift
+//  SecurityFeatures
+//
+//  Created by Adrian Somesan on 09.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SecurityFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecurityFeatures/SecurityFeaturesTests/SecurityFeaturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityFeaturesTests.swift
+//  SecurityFeaturesTests
+//
+//  Created by Adrian Somesan on 09.07.2025.
+//
+
+import Testing
+@testable import SecurityFeatures
+
+struct SecurityFeaturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
