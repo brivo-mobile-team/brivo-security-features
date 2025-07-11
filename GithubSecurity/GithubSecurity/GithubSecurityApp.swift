ea611a5b4917612eeaf6d650e3080c11d6f96df1
Adrian Somesan <adrian.somesan@brivo.com>
2025-07-11 12:44:17 +0300

    add secret

--- GithubSecurity/GithubSecurity/GithubSecurityApp.swift
+++ GithubSecurity/GithubSecurity/GithubSecurityApp.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct GithubSecurityApp: App {
     let firebaseClientID = "a111aa11-a111-11a1-aaaa-aa1111a11a11"
     let firebaseClientID1 = "a111aa11-a111-11a1-aaaa-aa1111a11a11"
+
+    let aws = "AKIAXZDQCENYX45KWNK"
+    let key = "AeloZNCOMU3PU+84RR6l0lkUYrnCvfXpUVVCQLlW"
+
     var body: some Scene {
         WindowGroup {
             ContentView()
