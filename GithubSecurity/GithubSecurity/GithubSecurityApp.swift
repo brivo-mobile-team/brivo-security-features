f6f70d02a99a0789861cbeb4f0abcb2b2e82eab5
Adrian Somesan <adrian.somesan@brivo.com>
2025-07-11 12:19:22 +0300

    test terminal

--- GithubSecurity/GithubSecurity/GithubSecurityApp.swift
+++ GithubSecurity/GithubSecurity/GithubSecurityApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct GithubSecurityApp: App {
     let firebaseClientID = "a111aa11-a111-11a1-aaaa-aa1111a11a11"
+    let firebaseClientID1 = "a111aa11-a111-11a1-aaaa-aa1111a11a11"
     var body: some Scene {
         WindowGroup {
             ContentView()
