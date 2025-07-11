55ee069a0e5ef5e0f91bdfb814655e2f51747c2c
Adrian Somesan <adrian.somesan@brivo.com>
2025-07-11 12:15:11 +0300

    test terminal

--- GithubSecurity/GithubSecurity/ContentView.swift
+++ GithubSecurity/GithubSecurity/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
     let firebaseClientID10 = "b111aa11-a111-11a1-aaaa-aa1111a11a12"
     let firebaseClientID11 = "b111aa11-a111-11a1-aaaa-aa1111a11a12"
     let firebaseClientID12 = "b111aa11-a111-11a1-aaaa-aa1111a11a12"
+    let firebaseClientID13 = "b111aa11-a111-11a1-aaaa-aa1111a11a12"
 
     var body: some View {
         VStack {
